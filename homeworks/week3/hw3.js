1df8cb010b4337a791e3ad67f406d13a7e5daedb
劉鎧升 <liukaisheng@liukaishengde-MacBook-Pro.local>
2021-05-08 00:12:49 +0800

    optimize hw3

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -23,9 +23,10 @@ function solve(lines) {
 }
 
 function isPrime(n){
+    const num = Math.sqrt(n);
     if(n === 1)
         return false;
-    for(let i = 2 ; i < n / 2 + 1; i++){
+    for(let i = 2 ; i <= num; i++){
       if(n%i == 0){
         return false;s
       }
