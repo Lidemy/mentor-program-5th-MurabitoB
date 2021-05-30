d5d8001703852b7b1ca5b58b2e05bace2938c88f
MurabitoB <enemarykawai@gmail.com>
2021-05-30 22:40:09 +0800

    完成 hw2 hw3

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -6,7 +6,7 @@ if (args.length > 2) {
 
     switch (args[2]) {
         case 'list':
-            request(baseUrl + '/books?_limit=20', function (error, response, body) {
+            request(baseUrl + '/books?_limit=100', function (error, response, body) {
             const books = JSON.parse(body);
             for (let book of books) {
                 console.log(`${book.id} ${book.name}`);
@@ -20,9 +20,7 @@ if (args.length > 2) {
             });
             break;
         case 'delete':
-            request.delete(baseUrl + `/books/${args[3]}`, function (error, response, body) {
-            console.log(body);
-            });
+            request.delete(baseUrl + `/books/${args[3]}`);
             break;
         case 'create':
             request.post(baseUrl + `/books`).form({name:args[3]});
